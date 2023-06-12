d07ea238d8abae054b04822f1b86977c4035d840
Neyoki <77333688+NeyokiCat@users.noreply.github.com>
2023-06-12 20:21:44 +0800

    20230612

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import os
+import pynvml
+#import argparse
 PATH = os.path.dirname(__file__)
 PATH_to_Collection = os.path.join(os.path.dirname(__file__),"COllections")
+
+#parser = argparse.ArgumentParser(
+#                    prog='ColabStation',
+#                    description='A platform for collection of Ai tools',
+#                    epilog='')
+#
+#parser.add_argument("-h","--he")
+
+def scan_GPU():
+    pynvml.nvmlInit()
+    deviceCount = pynvml.nvmlDeviceGetCount()
+    for i in range(deviceCount):
+        handle = pynvml.nvmlDeviceGetHandleByIndex(i)
+        meminfo = pynvml.nvmlDeviceGetMemoryInfo(handle)
+        print("GPU", i, ":", pynvml.nvmlDeviceGetName(handle), meminfo.total)
+    return i
 
 def find_env(path): # Search python.exe under certain path
     for root, dirs, files in os.walk(path):
@@ -19,4 +37,6 @@ def scan_for_env(): # Search all python.exe location under Collections
             find_env(os.path.join(PATH_to_Collection,dir))
             print("")
 
+## Test area ##
 # scan_for_env()
+# scan_GPU()
